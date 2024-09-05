08a58cbc4d85facf2eb010de35ae88d79f481084
Salvatore Correnti <servator.correnti@gmail.com>
2024-09-05 21:14:12 +0200

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -13,11 +13,13 @@ git clone "$FASTFLOW_REPO"
 # Download openmpi-5.0.3
 echo "Downloading OpenMPI 5.0.3 ..."
 OPENMPI_URL="https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.3.tar.gz"
-wget -O "openmpi-5.0.3" "$OPENMPI_URL"
+wget -O "openmpi-5.0.3.tar.gz" "$OPENMPI_URL"
 
 # Extract openmpi-5.0.3
 echo "Extracting OpenMPI 5.0.3 ..."
-OPENMPI_TAR="openmpi-5.0.3/openmpi-5.0.3.tar.gz"
-tar -xzf "$OPENMPI_TAR" -C "openmpi-5.0.3"
+OPENMPI_TAR="openmpi-5.0.3.tar.gz"
+OPENMPI_DIR="openmpi-5.0.3"
+mkdir -p "$OPENMPI_DIR"
+tar -xzf "$OPENMPI_TAR" -C "$OPENMPI_DIR"
 
 echo "Download, clone and extraction operations completed."
